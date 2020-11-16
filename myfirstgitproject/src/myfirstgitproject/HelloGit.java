a68fc43b888dbcf13b9a3332cd2b8dd4874777d0
studentcse12345 <studentcse12345@gmail.com>
2020-11-16 23:15:59 +0530

    This is a sample git commit and my fist commit

---
+++ myfirstgitproject/src/myfirstgitproject/HelloGit.java
@@ -0,0 +1,11 @@
+package myfirstgitproject;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git Repository");
+
+	}
+
+}
